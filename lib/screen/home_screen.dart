08435af9dd91c1cfffca90e7eee1b8f72b37e036
tlgusdl03 <tlgusdl03@gmail.com>
2024-01-04 15:22:28 +0900

    콜백함수 이용하여 홈버튼 구현

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+final homeUrl = Uri.parse('https://blog.codefactory.ai');
+
 class HomeScreen extends StatelessWidget {
-  WebViewController controller = WebViewController();
+  // .. 실행한 결과를 반환하는 대신 실행한 대상을 반환하라는 의미
+  WebViewController controller = WebViewController()
+  ..loadRequest(homeUrl);
 
   HomeScreen({super.key});
 
@@ -13,6 +17,16 @@ class HomeScreen extends StatelessWidget {
         backgroundColor: Colors.orange,
         title: Text('Code Factory'),
         centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: (){
+              controller.loadRequest(homeUrl);
+            },
+            icon: Icon(
+              Icons.home,
+            )
+          )
+        ],
       ),
       body: WebViewWidget(
         controller: controller,
