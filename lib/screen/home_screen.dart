be882f6bc42b9cd724193a958ef520fc5b3a871f
tlgusdl03 <tlgusdl03@gmail.com>
2024-01-05 02:06:56 +0900

    pageview controller 추가

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,18 +12,32 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   Timer? timer;
-  
+  PageController controller = PageController(
+    initialPage: 0,
+  );
+
   @override
   void initState() {
     // TODO: implement initState
-    timer = Timer.periodic(Duration(seconds: 1), (timer) {
-      print('TImer!');
+    timer = Timer.periodic(Duration(seconds: 4), (timer) {
+      int currentPage = controller.page!.toInt();
+      int nextPage = currentPage + 1;
+      if (nextPage > 4) {
+        nextPage = 0;
+      }
+      controller.animateToPage(
+        nextPage,
+        duration: Duration(milliseconds: 400),
+        curve: Curves.linear,
+      );
     });
   }
-  
+
   @override
   void dispose() {
-    if(timer != null){
+    controller.dispose();
+
+    if (timer != null) {
       timer!.cancel();
     }
 
@@ -31,8 +46,10 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
     return Scaffold(
       body: PageView(
+        controller: controller,
         children: [1, 2, 3, 4, 5]
             .map(
               (e) => Image.asset(
