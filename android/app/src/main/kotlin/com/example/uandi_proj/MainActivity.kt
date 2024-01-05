e7d5b0d356e27f1b1b05f5a4cf237f9b23e43c50
tlgusdl03 <tlgusdl03@gmail.com>
2024-01-05 10:15:55 +0900

    UI 구성 완료

---
+++ android/app/src/main/kotlin/com/example/uandi_proj/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.uandi_proj
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:uandi_proj/home_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    )
+  );
+}
+
